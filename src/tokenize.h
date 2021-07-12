2f33455d375549515c4113a1b44c17ec500e9e5c
Daniel N. Baker <dnb@cs.jhu.edu>
2021-07-12 12:27:02 -0400

    Separate alphabet from tokenizer, constexpr-ify the alphabets

--- src/tokenize.h
+++ src/tokenize.h
@@ -1,105 +1,12 @@
 #include "pybind11/pybind11.h"
+#include "alphabet.h"
 #include "pybind11/numpy.h"
-#include <array>
 #include <string>
-#include <map>
 
-
-
-/*
- *
- * This 
- */
-
-struct CompressedAlphabet {
-    const char *name;
-    const char *setstr;
-    size_t nc;
-    bool padding;
-    size_t nchars() const {return nc + 1;} // One for padding
-    using LUType = std::array<uint8_t, 256>;
-    LUType lut;
-    static constexpr LUType make_lut(const char *s, const size_t nc, bool padding=false) {
-        LUType arr{0};
-        int id = padding;
-        for(size_t ci = 0, i = 0; i < nc; ++i, ++id) {
-            while(s[ci] && s[ci] != ',') {
-                const auto v = s[ci++];
-                arr[v] = arr[std::tolower(v)] = id;
-            }
-            if(s[ci] == ',') ++ci;
-        }
-        // Handle Pyrrolysine (P) by mapping it to Lysine (K) if unhandled
-        if(arr['P'] == 0) arr['P'] = arr['p'] = arr['K'];
-        // Handle SelenoCysteine (U) by mapping it to Cysteine if unhandled
-        if(arr['U'] == 0) arr['U'] = arr['u'] = arr['C'];
-        return arr;
-    }
-    constexpr uint8_t translate(char x) const {return lut[static_cast<uint8_t>(x)];}
-    static constexpr size_t ncommas(const char *s) {
-        size_t ret = 0, i = 0;
-        while(s[i]) ret += (s[i] == ','), ++i;
-        return ret;
-    }
-    constexpr CompressedAlphabet(const CompressedAlphabet &) = default;
-    constexpr CompressedAlphabet(CompressedAlphabet &&) = default;
-    constexpr CompressedAlphabet(const char *name, const char *s, bool padding=false): name(name), setstr(s), nc(ncommas(s)), padding(padding), lut(make_lut(s, nc, padding)) {
-    }
-};
-
-
-// Protein Alphabets
-const CompressedAlphabet AMINO20("Standard20", "A,C,D,E,F,G,H,I,K,L,M,N,P,Q,R,S,T,V,W,Y");
-
-const CompressedAlphabet SEB14("SE-B(14)", "A,C,D,EQ,FY,G,H,IV,KR,LM,N,P,ST,W");
-
-const CompressedAlphabet SEB10("SE-B(10)", "AST,C,DN,EQ,FY,G,HW,ILMV,KR,P");
-const CompressedAlphabet SEV10("SE-V(10)", "AST,C,DEN,FY,G,H,ILMV,KQR,P,W");
-const CompressedAlphabet SOLISD("Solis-D", "AM,C,DNS,EKQR,F,GP,HT,IV,LY,W");
-const CompressedAlphabet SOLISG("Solis-G", "AEFIKLMQRVW,C,D,G,H,N,P,S,T,Y");
-const CompressedAlphabet MURPHY("Murphy", "A,C,DENQ,FWY,G,H,ILMV,KR,P,ST");
-const CompressedAlphabet LIA10("Li-A(10)", "AC,DE,FWY,G,HN,IV,KQR,LM,P,ST");
-const CompressedAlphabet LIB10("Li-B(10)", "AST,C,DEQ,FWY,G,HN,IV,KR,LM,P");
-
-const CompressedAlphabet SEB8("SE-B(8)","AST,C,DHN,EKQR,FWY,G,ILMV,P");
-const CompressedAlphabet SEB6("SE-B(6)","AST,CP,DHNEKQR,FWY,G,ILMV");
-
-const CompressedAlphabet DAYHOFF("Dayhoff","AGPST,C,DENQ,FWY,HKR,ILMV");
-
-// DNA alphabets
-
-const CompressedAlphabet DNA4("DNA4", "A,C,G,T");
-const CompressedAlphabet DNA5("DNA5", "A,C,G,T,NMRWSYKVHDB");
-
-const CompressedAlphabet DNA2KETAMINE("DNA2", "ACM,KGT"); // Amino/Ketones
-const CompressedAlphabet DNA2PYRPUR("DNA2", "AGR,YCT"); // Purines/Pyrimidines
-
-
-// Source: Reference
-// Edgar, RC (2004) Local homology recognition and distance measures in linear time using compressed amino acid alphabets, NAR 32(1), 380-385. doi: 10.1093/nar/gkh180
-static const std::map<std::string, const CompressedAlphabet *> CAMAP = {
-    {"AMINO20", &AMINO20},
-    {"AMINO", &AMINO20},
-    {"PROTEIN", &AMINO20},
-    {"SEB8", &SEB8},
-    {"SEB10", &SEB10},
-    {"SEB14", &SEB14},
-    {"SEV10", &SEV10},
-    {"MURPHY", &MURPHY},
-    {"LIA10", &LIA10},
-    {"LIB10", &LIB10},
-    {"SEB6", &SEB6},
-    {"DAYHOFF", &DAYHOFF},
-
-    {"KETO", &DNA2KETAMINE},
-    {"PURPYR", &DNA2PYRPUR},
-    {"DNA4", &DNA4},
-    {"DNA", &DNA4},
-    {"DNA5", &DNA5}
-};
+using namespace alph;
 
 struct Tokenizer {
-    const CompressedAlphabet *ca_;
+    const Alphabet *ca_;
     const bool include_eos_;
     const bool include_bos_;
     const bool zero_onehot_pad_;
@@ -112,7 +19,7 @@ struct Tokenizer {
     int eos() const {if(!include_eos_) return -1; return bos() + 1;}
     int pad() const {return eos() + 1;}
     // Always included: padding
-    Tokenizer(const CompressedAlphabet &ca, bool eos=false, bool bos=false, bool zero_onehot_pad=false): ca_(&ca), include_eos_(eos), include_bos_(bos), zero_onehot_pad_(zero_onehot_pad) {
+    Tokenizer(const Alphabet &ca, bool eos=false, bool bos=false, bool zero_onehot_pad=false): ca_(&ca), include_eos_(eos), include_bos_(bos), zero_onehot_pad_(zero_onehot_pad) {
     }
     Tokenizer(std::string key, bool include_eos, bool include_bos, bool zohpad): include_eos_(include_eos), include_bos_(include_bos), zero_onehot_pad_(zohpad) {
         std::transform(key.begin(), key.end(), key.begin(),[](auto x){return std::toupper(x);});
