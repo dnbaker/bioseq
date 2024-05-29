cb62463263c6f113305769268987744ff50d2388
Daniel N. Baker <dnb@cs.jhu.edu>
2024-05-29 13:45:17 -0700

    Add submodule for spoa 4.1.0

--- src/bioseq.cpp
+++ src/bioseq.cpp
@@ -2,8 +2,10 @@
 
 void init_omp_helpers(py::module &m);
 void init_fxstats(py::module &m);
+void init_poa(py::module &m);
 PYBIND11_MODULE(cbioseq, m) {
     init_tokenize(m);
     init_omp_helpers(m);
     init_fxstats(m);
+    init_poa(m);
 }
